96c6f2d1b10049cecb487bd52465d0f9b81eff84
Silvan Buedenbender <silvancodes@gmail.com>
2022-05-05 23:40:24 +0200

    Add party intent

--- __init__.py
+++ __init__.py
@@ -5,15 +5,20 @@ ESP_HOST = 'http://esp32.local'
 
 POWER_ROUTE = '/power'
 COLOR_ROUTE = '/color'
+COLOR_ROUTE = '/party'
 
 POWER_REQUEST = urllib.request.Request(url=f'{ESP_HOST}{POWER_ROUTE}', method='POST')
 COLOR_REQUEST = urllib.request.Request(url=f'{ESP_HOST}{COLOR_ROUTE}', method='POST')
+PARTY_REQUEST = urllib.request.Request(url=f'{ESP_HOST}{PARTY_ROUTE}', method='POST')
 
 def toggle_power():
     urllib.request.urlopen(POWER_REQUEST)
 
 def toggle_color():
     urllib.request.urlopen(COLOR_REQUEST)
+
+def toggle_party():
+    urllib.request.urlopen(PARTY_REQUEST)
 
 
 class LightControl(MycroftSkill):
@@ -22,18 +27,23 @@ class LightControl(MycroftSkill):
 
     @intent_handler('control.light.on.intent')
     def handle_light_on(self, message):
-        toggle_power()
         self.speak_dialog('control.light.on')
+        toggle_power()
 
     @intent_handler('control.light.off.intent')
     def handle_light_off(self, message):
-        toggle_power()
         self.speak_dialog('control.light.off')
+        toggle_power()
 
     @intent_handler('control.light.color.intent')
     def handle_light_color(self, message):
-        toggle_color()
         self.speak_dialog('control.light.color')
+        toggle_color()
+
+    @intent_handler('control.light.party.intent')
+    def handle_light_color(self, message):
+        self.speak_dialog('control.light.party')
+        toggle_party()
 
 def create_skill():
     return LightControl()
